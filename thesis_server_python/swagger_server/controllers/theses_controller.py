0a53e5bd31379465931c171ef7f48b7f09431eef
ffranziskus <ffranziskus13@gmx.de>
2025-03-21 08:43:25 +0100

    partially implemented server

--- thesis_server_python/swagger_server/controllers/theses_controller.py
+++ thesis_server_python/swagger_server/controllers/theses_controller.py
@@ -6,7 +6,11 @@ from swagger_server.models.thesis import Thesis  # noqa: E501
 from swagger_server.models.thesis_id import ThesisId  # noqa: E501
 from swagger_server.models.thesis_summary import ThesisSummary  # noqa: E501
 from swagger_server import util
-from swagger_server.DatabaseConnection.PostgreSQL import ThesisDB, getSession
+from swagger_server.DatabaseConnection.PostgreSQL import ThesisDB, engine, convertToThesisSummary, convertToThesis, thesisToDict
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+from sqlalchemy.exc import NoResultFound
+
 
 def theses_get():  # noqa: E501
     """List theses summaries
@@ -16,12 +20,12 @@ def theses_get():  # noqa: E501
 
     :rtype: List[ThesisSummary]
     """
-    session = getSession()
-    results = session.query(ThesisDB).all()
-    return [ThesisSummary()]
+    with Session(engine) as session:
+        results = session.query(ThesisDB).all()
+    return [convertToThesisSummary(t) for t in results]
 
 
-def theses_id_get(id):  # noqa: E501
+def theses_id_get(id_):  # noqa: E501
     """Get information for a specific thesis
 
      # noqa: E501
@@ -32,8 +36,22 @@ def theses_id_get(id):  # noqa: E501
     :rtype: InlineResponse201
     """
     if connexion.request.is_json:
-        id = ThesisId.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+        id_ = ThesisId.from_dict(connexion.request.get_json())  # noqa: E501
+
+    id = id_.id if isinstance(id_, ThesisId) else id_
+
+    stmt = select(ThesisDB).where(ThesisDB.thesisId == id)
+    with Session(engine) as session:
+        try:
+            thesis = session.scalars(stmt).one()
+        except NoResultFound as e:
+            return Error("Unknown thesis id.")
+        except Exception as e:
+            return Error("An error has occured.")
+    
+    return InlineResponse201(
+        convertToThesis(thesis)
+    )
 
 
 def theses_id_put(id, body=None):  # noqa: E501
@@ -93,4 +111,8 @@ def theses_post(body=None):  # noqa: E501
     """
     if connexion.request.is_json:
         body = Thesis.from_dict(connexion.request.get_json())  # noqa: E501
+
+    print(type(body))
+    print(body)
+
     return 'do some magic!'
